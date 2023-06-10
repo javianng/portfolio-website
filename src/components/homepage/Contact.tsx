a7d8b7280e13edf7ee31ae23b69a0950a783085a
javianng <j4vianz01@gmail.com>
2023-06-11 00:08:18 +0700

    feat updated contact page

--- src/components/homepage/Contact.tsx
+++ src/components/homepage/Contact.tsx
@@ -1,30 +1,39 @@
 import profilephoto from 'src/assets/profilephoto.jpg'
 import Image from "next/image";
+import { SocialIcon } from 'react-social-icons';
 
 export default function Contact() {
     return (
         <>
-            <div className='flex justify-center'>
+            <div className='flex justify-center pb-6'>
                 <div className="
                     w-11/12 max-w-5xl"
                 >
-                    <div className="grid grid-cols-2 mb-4">
-                        <div>
-                            <h1 className="text-8xl font-bold mb-9">
-                                Contact Me
+                    <div className="flex mb-4">
+                        <div className='w-1/2'>
+                            <h1 className="text-8xl font-bold mb-9 ">
+                                My Contact
                             </h1>
                             <p>
                                 Aliquip sint sunt voluptate consectetur eiusmod exercitation anim aliquip ut tempor. Voluptate aliquip pariatur qui ullamco fugiat proident irure excepteur deserunt anim eu reprehenderit cupidatat. Proident sint enim ut cupidatat nisi proident nostrud. Sunt cupidatat consequat anim in ut sit deserunt labore id nulla est tempor ullamco dolor. Adipisicing ut eiusmod aute. Ipsum qui non minim adipisicing id anim officia sunt nulla irure minim nulla in excepteur.
                             </p>
 
-
                         </div>
-                        <div className='object-contain ml-9 justify-end '>
+                        <div className='object-contain ml-9 justify-end w-1/2'>
                             <Image
                                 src={profilephoto}
                                 alt=""
                                 className=''
                             />
+                        </div>
+                    </div>
+                    <div className='flex'>
+                        <div className='grid grid-flow-col gap-3'>
+                            <SocialIcon url="https://twitter.com/jaketrent" />
+                            <SocialIcon url="https://www.linkedin.com/in/javianngzh/" />
+                            <SocialIcon url="https://github.com/javianng" />
+                            <SocialIcon url="mailto:javian.ng.z.h@gmail.com" />
+                            <SocialIcon url="https://t.me/javianng" />
                         </div>
                     </div>
                 </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,50 @@
 import { type NextPage } from "next";
+
 import Navbar from "~/components/Navbar";
+import Footer from "~/components/Footer";
+
 import HomePage from "~/components/homepage/HomePage";
 import Expertise from "~/components/homepage/Expertise";
 import Work from "~/components/homepage/Work";
 import Contact from "~/components/homepage/Contact";
 import Experience from "~/components/homepage/Experience";
+
 import NextButton from "~/components/Button";
 
 const Home: NextPage = () => {
   return (
-    <div className="m-4">
-      <Navbar />
-      <HomePage />
+    <>
+      <div className="m-4">
+        <Navbar />
+        <HomePage />
 
-      <div className="flex justify-center h-[10vh] mt-5">
-        <NextButton url="apple" />
+        <div className="flex justify-center h-[10vh] mt-5">
+          <NextButton url="apple" />
+        </div>
+
+        <Expertise />
+
+        <div className="flex justify-center h-[10vh] mt-5">
+          <NextButton url="apple" />
+        </div>
+
+        <Work />
+
+        <div className="flex justify-center h-[10vh] mt-5">
+          <NextButton url="apple" />
+        </div>
+
+        <Experience />
+
+        <div className="flex justify-center h-[10vh] mt-5">
+          <NextButton url="apple" />
+        </div>
+
+        <Contact />
+
       </div>
-
-      <Expertise />
-
-      <div className="flex justify-center h-[10vh] mt-5">
-        <NextButton url="apple" />
-      </div>
-
-      <Work />
-
-      <div className="flex justify-center h-[10vh] mt-5">
-        <NextButton url="apple" />
-      </div>
-
-      <Experience />
-
-      <div className="flex justify-center h-[10vh] mt-5">
-        <NextButton url="apple" />
-      </div>
-
-      <Contact />
-    </div>
+      <Footer />
+    </>
   );
 };
 
