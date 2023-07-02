3b1583c5b9c3d762e91cac5436f15adabc3a9abd
javianng <j4vianz01@gmail.com>
2023-07-03 03:41:45 +0800

    commit

--- src/components/homepage/Expertise/ExpertiseDetails.tsx
+++ src/components/homepage/Expertise/ExpertiseDetails.tsx
@@ -2,22 +2,6 @@ import { ComputerDesktopIcon } from '@heroicons/react/24/outline'
 import { CpuChipIcon } from '@heroicons/react/24/outline'
 import { PresentationChartLineIcon } from '@heroicons/react/24/outline'
 import { DevicePhoneMobileIcon } from '@heroicons/react/24/outline'
-import Image from "next/image";
-
-// logo
-import NextJS from "src/assets/logos/NextJS.svg"
-import React from "src/assets/logos/React.svg"
-import CSS from "src/assets/logos/CSS.svg"
-
-import Prisma from "src/assets/logos/Prisma.svg"
-import mySQL from "src/assets/logos/mySQL.svg"
-
-import Python from "src/assets/logos/Python.svg"
-import R from "src/assets/logos/R.svg"
-
-import Java from "src/assets/logos/Java.svg"
-import Javascript from "src/assets/logos/Javascript.svg"
-import Typescript from "src/assets/logos/Typescript.svg"
 
 export const EXPERTISE_COMPONENT_DETAILS = [
     {
@@ -27,12 +11,7 @@ export const EXPERTISE_COMPONENT_DETAILS = [
         description: "I like to code things from scratch, and enjoy bringing ideas to life in the browser.",
         icon: <ComputerDesktopIcon />,
         color: "hover:bg-brand-100",
-        skillLogo:
-            <>
-                <Image src={NextJS as string} alt={'NextJS'} className='h-fit' />
-                <Image src={React as string} alt={'React'} className='h-fit' />
-                <Image src={CSS as string} alt={'CSS'} className='h-fit' />
-            </>
+
     },
     {
         name: "Backend Development",
@@ -41,11 +20,7 @@ export const EXPERTISE_COMPONENT_DETAILS = [
         description: "Experienced with working on backend development.",
         icon: <CpuChipIcon />,
         color: "hover:bg-brand-200 hover:text-white",
-        skillLogo:
-            <>
-                <Image src={Prisma as string} alt={'Prisma'} className='h-fit' />
-                <Image src={mySQL as string} alt={'mySQL'} className='h-fit' />
-            </>
+
     },
     {
         name: "Data Analytics",
@@ -54,11 +29,7 @@ export const EXPERTISE_COMPONENT_DETAILS = [
         description: "Analytical and detailed oriented, with knowledge and practical experience in data analytics.",
         icon: <PresentationChartLineIcon />,
         color: "hover:bg-brand-300 hover:text-white",
-        skillLogo:
-            <>
-                <Image src={Python as string} alt={'Python'} className='h-fit' />
-                <Image src={R as string} alt={'R'} className='h-fit' />
-            </>
+
     },
     {
         name: "Software Development",
@@ -67,11 +38,6 @@ export const EXPERTISE_COMPONENT_DETAILS = [
         description: "Knowledge in both functional and object-oriented programming languages.",
         icon: <DevicePhoneMobileIcon />,
         color: "hover:bg-brand-100",
-        skillLogo:
-            <>
-                <Image src={Java as string} alt={'Java'} className='h-fit' />
-                <Image src={Javascript as string} alt={'JavaScript'} className='h-fit' />
-                <Image src={Typescript as string} alt={'TypeScript'} className='h-fit' />
-            </>
+
     },
 ];
