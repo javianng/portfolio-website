5d011239d7e006ff7623cacf5a9f09c9b6a7b686
javianng <j4vianz01@gmail.com>
2023-07-03 12:28:03 +0800

    commit

--- src/components/homepage/myExpertise/Expertise.tsx
+++ src/components/homepage/myExpertise/Expertise.tsx
@@ -10,6 +10,7 @@ type ExpertiseComponentProps = {
     description: string;
     icon: ReactNode;
     color: string;
+    logo: ReactNode;
 };
 
 const ExpertiseComponent = ({
@@ -18,37 +19,42 @@ const ExpertiseComponent = ({
     subtitle,
     description,
     icon,
-    color }: ExpertiseComponentProps) => {
+    color,
+    logo }: ExpertiseComponentProps) => {
     return (
         <div className={`group tracking-tight p-3 border-black border-2 ${color} transition delay-75 ease-in-out`}>
-            <div className='px-3 py-8'>
-
-                <div className='flex align-top pb-4 brand'>
-                    <div className='w-16 mr-4'>
-                        {icon}
+            <div className='px-3 py-8 flex justify-between'>
+                <div>
+                    <div className='flex align-top pb-4 brand'>
+                        <div className='w-16 mr-4'>
+                            {icon}
+                        </div>
+                        <div>
+                            <div className='m-2'></div>
+                            <h2 className={`${decorationColor} text-4xl font-bold mb-2 underline decoration-solid decoration-8`}>
+                                {name}
+                            </h2>
+                            <h3 className="text-4xl font-bold">
+                                {subtitle}
+                            </h3>
+                        </div>
                     </div>
-                    <div>
-                        <div className='m-2'></div>
-                        <h2 className={`${decorationColor} text-4xl font-bold mb-2 underline decoration-solid decoration-8`}>
-                            {name}
-                        </h2>
-                        <h3 className="text-4xl font-bold">
-                            {subtitle}
-                        </h3>
+                    <div className='flex'>
+                        <div className='grid grid-cols-1'>
+                            <p className='my-[-4px] text-gray-500 font-mono'>
+                                &lt;h1&gt;
+                            </p>
+                            <p className='text-l border-l-[1.5px] ml-5 pl-4 py-3 border-gray-500'>
+                                {description}
+                            </p>
+                            <p className='my-[-4px] text-gray-500 font-mono'>
+                                &lt;h1/&gt;
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div className='flex'>
-                    <div className='grid grid-cols-1'>
-                        <p className='my-[-4px] text-gray-500 font-mono'>
-                            &lt;h1&gt;
-                        </p>
-                        <p className='text-l border-l-[1.5px] ml-5 pl-4 py-3 border-gray-500'>
-                            {description}
-                        </p>
-                        <p className='my-[-4px] text-gray-500 font-mono'>
-                            &lt;h1/&gt;
-                        </p>
-                    </div>
+                <div className='grid grid-rows-3'>
+                    {logo}
                 </div>
             </div>
         </div>
--- src/components/homepage/myExpertise/ExpertiseDetails.tsx
+++ src/components/homepage/myExpertise/ExpertiseDetails.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
+
 import { ComputerDesktopIcon } from '@heroicons/react/24/outline'
 import { CpuChipIcon } from '@heroicons/react/24/outline'
 import { PresentationChartLineIcon } from '@heroicons/react/24/outline'
 import { DevicePhoneMobileIcon } from '@heroicons/react/24/outline'
+
+import CSS from "src/assets/logos/CSS.svg"
+import NextJS from "src/assets/logos/NextJS.svg"
+import React from "src/assets/logos/React.svg"
 
 export const EXPERTISE_COMPONENT_DETAILS = [
     {
@@ -11,7 +17,12 @@ export const EXPERTISE_COMPONENT_DETAILS = [
         description: "I like to code things from scratch, and enjoy bringing ideas to life in the browser.",
         icon: <ComputerDesktopIcon />,
         color: "hover:bg-brand-100",
-
+        logo:
+            <>
+                <Image src={CSS} alt={"CSS"} />
+                <Image src={NextJS} alt={"NextJS"} />
+                <Image src={React} alt={"React"} />
+            </>
     },
     {
         name: "Backend Development",
@@ -20,7 +31,12 @@ export const EXPERTISE_COMPONENT_DETAILS = [
         description: "Experienced with working on backend development.",
         icon: <CpuChipIcon />,
         color: "hover:bg-brand-200 hover:text-white",
-
+        logo:
+            <>
+                <Image src={CSS} alt={"CSS"} />
+                <Image src={NextJS} alt={"NextJS"} />
+                <Image src={React} alt={"React"} />
+            </>
     },
     {
         name: "Data Analytics",
@@ -29,7 +45,12 @@ export const EXPERTISE_COMPONENT_DETAILS = [
         description: "Analytical and detailed oriented, with knowledge and practical experience in data analytics.",
         icon: <PresentationChartLineIcon />,
         color: "hover:bg-brand-300 hover:text-white",
-
+        logo:
+            <>
+                <Image src={CSS} alt={"CSS"} />
+                <Image src={NextJS} alt={"NextJS"} />
+                <Image src={React} alt={"React"} />
+            </>
     },
     {
         name: "Software Development",
@@ -38,6 +59,11 @@ export const EXPERTISE_COMPONENT_DETAILS = [
         description: "Knowledge in both functional and object-oriented programming languages.",
         icon: <DevicePhoneMobileIcon />,
         color: "hover:bg-brand-100",
-
+        logo:
+            <>
+                <Image src={CSS} alt={"CSS"} />
+                <Image src={NextJS} alt={"NextJS"} />
+                <Image src={React} alt={"React"} />
+            </>
     },
 ];
