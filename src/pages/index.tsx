e9f50650025dea3a7eff2802119a457b01c99189
javianng <j4vianz01@gmail.com>
2023-07-03 11:59:11 +0800

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { type NextPage } from "next";
 import PageLayout from "~/components/common/PageLayout";
 
 import HomePage from "~/components/homepage/HomePage";
-import Expertise from "~/components/homepage/Expertise";
+import Expertise from "~/components/homepage/Expertise/Expertise";
 import Work from "~/components/homepage/Work/Work";
 import Contact from "~/components/homepage/Contact";
 import Experience from "~/components/homepage/Experience";
