bf0f4a7a1f8cb25e42aad890ce8db049b5f0a0b7
javianng <j4vianz01@gmail.com>
2023-07-03 12:06:05 +0800

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,27 @@
 const NAVBAR_COMPONENT_DETAILS = [
     {
-        name: "HOME",
+        name: "Home",
         href: "#Home"
     },
     {
-        name: "EXPERTISE",
+        name: "About Me",
+        href: "#AboutMe"
+    },
+    {
+        name: "Expertise",
         href: "#Expertise"
     },
     {
-        name: "WORK",
+        name: "Work",
         href: "#Work"
     },
     {
-        name: "EXPERIENCE",
+        name: "Experience",
         href: "#Experience"
     },
     {
-        name: "CONTACT",
-        href: "#Contact"
+        name: "Testimonials",
+        href: "#Testimonials"
     },
 ];
 
@@ -30,7 +34,7 @@ const NavbarComponent = ({ name, href }: NavbarComponentProps) => {
     return (
         <a
             href={href}
-            className="flex w-36 justify-center"
+            className="flex w-36 justify-center uppercase"
         >
             {name}
         </a>
