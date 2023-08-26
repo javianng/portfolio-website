3395968a9411d792a6932588e4c4b79f08368c35
javianng <j4vianz01@gmail.com>
2023-08-26 14:48:01 +0800

    feat: implement swiperjs

--- src/components/homepage/Testimonials.tsx
+++ src/components/homepage/Testimonials.tsx
@@ -5,8 +5,18 @@ import PageContainer from "../common/PageContainer";
 import TitleContainer from "../common/TitleContainer";
 import { TESTIMONIAL_DETAILS } from "./TestimonialDetails";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Navigation, Pagination } from 'swiper/modules';
+
 type CarouselCardProps = {
-    reference: string,
     image: string | StaticImageData;
     quote: ReactNode;
     personel: string;
@@ -14,15 +24,14 @@ type CarouselCardProps = {
 }
 
 function CarouselCard({
-    reference,
     image,
     quote,
     personel,
     organisation }: CarouselCardProps) {
     return (
-        <div id={reference} className="carousel-item w-full">
+        <div className="carousel-item w-full py-9 justify-center">
             <div>
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center mx-16">
                     <div className="avatar pb-8">
                         <div className=" w-36 mask mask-squircle outline-2 ">
                             <Image src={image} alt={""} />
@@ -54,16 +63,25 @@ export default function Testimonials() {
             <p className="text-center py-4">
                 Some nice things people have said about me...
             </p>
-            <div className="carousel w-full py-9">
+            <Swiper
+                navigation={true}
+                pagination={{
+                    dynamicBullets: true,
+                }}
+                modules={[Navigation, Pagination]}
+                style={{
+                    "--swiper-pagination-color": "#395B64",
+                    "--swiper-navigation-color": "#395B64",
+                }}
+            >
+
                 {TESTIMONIAL_DETAILS.map((cardDetails, i) => (
-                    <CarouselCard key={i} {...cardDetails} />
+                    <SwiperSlide>
+                        <CarouselCard key={i} {...cardDetails} />
+                    </SwiperSlide>
                 ))}
-            </div>
-            <div className="flex justify-center w-full gap-2">
-                <a href="#item1" className="btn btn-xs">1</a>
-                <a href="#item2" className="btn btn-xs">2</a>
-            </div>
-            <div className="h-[5vh]" />
+            </Swiper>
+            <div className="h-[4vh]" />
         </PageContainer>
     )
 }
