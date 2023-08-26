eda8b5fa19b1961c918ac61837e0731aaf4952a5
javianng <j4vianz01@gmail.com>
2023-08-26 14:10:37 +0800

    feat: implement mapping for carousel

---
+++ src/components/homepage/TestimonialDetails.tsx
@@ -0,0 +1,32 @@
+import zenacademy from "src/assets/testimony/zenacademy.png"
+
+export const TESTIMONIAL_DETAILS = [
+    {
+        reference: "item1",
+        image: zenacademy,
+        quote:
+            <div>
+                <span>
+                    &quot;A well- liked and hardworking fighter, Javian trained hard and persevered through the tough trainings which was physically and mentally exhausting.He gave his 100 % during trainings and never had his head down despite losing to his well - experienced black - belt seniors numerous times, always standing back up to fight back.
+                </span >
+                <br />
+                <span>
+                    His efforts none the less paid off when he represented the club in 4 separate tournaments in 2018, clinching 3 gold medals in the National Taekwondo Kyorugi Championships 2018 Ambassador Cup, Singapore National Games, Singapore Youth Olympic Festival and a bronze for the 6th Daedo Taekwondo Open Championships 2018. This attest for his passion and pursuit for excellence in things outside of academics.&quot;
+                </span>
+            </div>,
+        personel: "Zen Academy",
+        organisation: "Taekwondo Club",
+    },
+    {
+        reference: "item2",
+        image: zenacademy,
+        quote:
+            <div>
+                <span>
+                    2
+                </span >
+            </div>,
+        personel: "Zen ",
+        organisation: "Taekwondo Club",
+    },
+]
--- src/components/homepage/Testimonials.tsx
+++ src/components/homepage/Testimonials.tsx
@@ -1,7 +1,45 @@
+import Image, { StaticImageData } from "next/image";
+import { ReactNode } from "react";
+
 import PageContainer from "../common/PageContainer";
-import Image from "next/image";
-import zenacademy from "src/assets/testimony/zenacademy.png"
 import TitleContainer from "../common/TitleContainer";
+import { TESTIMONIAL_DETAILS } from "./TestimonialDetails";
+
+type CarouselCardProps = {
+    reference: string,
+    image: string | StaticImageData;
+    quote: ReactNode;
+    personel: string;
+    organisation: string;
+}
+
+function CarouselCard({
+    reference,
+    image,
+    quote,
+    personel,
+    organisation }: CarouselCardProps) {
+    return (
+        <div id={reference} className="carousel-item w-full">
+            <div>
+                <div className="flex flex-col items-center">
+                    <div className="avatar pb-8">
+                        <div className=" w-36 mask mask-squircle outline-2 ">
+                            <Image src={image} alt={""} />
+                        </div>
+                    </div>
+                    {quote}
+                    <p className="pt-5 font-extrabold">
+                        {personel}
+                    </p>
+                    <p>
+                        {organisation}
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default function Testimonials() {
     return (
@@ -17,31 +55,13 @@ export default function Testimonials() {
                 Some nice things people have said about me...
             </p>
             <div className="carousel w-full py-9">
-                <div id="item1" className="carousel-item w-full">
-                    <div className="flex flex-col items-center">
-                        <div className="avatar pb-8">
-                            <div className=" w-36 mask mask-squircle outline-2 ">
-                                <Image src={zenacademy} alt={""} />
-                            </div>
-                        </div>
-                        <p>
-                            &quot;A well-liked and hardworking fighter, Javian trained hard and persevered through the tough trainings which was physically and mentally exhausting. He gave his 100% during trainings and never had his head down despite losing to his well-experienced black-belt seniors numerous times, always standing back up to fight back.
-                        </p>
-                        <br />
-                        <p>
-                            His efforts none the less paid off when he represented the club in 4 separate tournaments in 2018, clinching 3 gold medals in the National Taekwondo Kyorugi Championships 2018 Ambassador Cup, Singapore National Games, Singapore Youth Olympic Festival and a bronze for the 6th Daedo Taekwondo Open Championships 2018. This attest for his passion and pursuit for excellence in things outside of academics.&quot;
-                        </p>
-                        <p className="pt-5 font-extrabold">
-                            Zen Academy
-                        </p>
-                        <p>
-                            Taekwondo Club
-                        </p>
-                    </div>
-                </div>
+                {TESTIMONIAL_DETAILS.map((cardDetails, i) => (
+                    <CarouselCard key={i} {...cardDetails} />
+                ))}
             </div>
             <div className="flex justify-center w-full gap-2">
                 <a href="#item1" className="btn btn-xs">1</a>
+                <a href="#item2" className="btn btn-xs">2</a>
             </div>
             <div className="h-[5vh]" />
         </PageContainer>
