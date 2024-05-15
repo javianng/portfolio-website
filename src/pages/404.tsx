6aedbbec9dcfdbb9dcab64f8214af411cf806bb8
Javian Ng <j4vianz01@gmail.com>
2024-05-15 12:16:41 +0800

    comit

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,26 +1,10 @@
-import Link from "next/link";
-import Head from "next/head";
-import { CircleX } from "lucide-react";
-import { PageLayoutProps } from "~/components/common/PageLayout";
 import ErrorPage from "~/components/common/ErrorPage";
+import { PageLayoutProps } from "~/components/common/PageLayout";
 
 export default function Page404(props: PageLayoutProps) {
-  const {
-    title = "Javian Ng",
-    description = "Part-Time Developer, Part-Time Business Analyst, Full-Time Curious Adventurer",
-  } = props;
-
-  // Render the 404 page
   return (
-    <>
-      <Head>
-        <title>{title}</title>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <meta content={description} name="description" />
-      </Head>
-      <main className="flex h-screen items-center justify-center bg-brand-300">
-        <ErrorPage />
-      </main>
-    </>
+    <main className="flex h-screen items-center justify-center bg-brand-300">
+      <ErrorPage />
+    </main>
   );
 }
